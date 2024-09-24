ba1a4c85f09c5624ad423b53c585d7ec30511662
obisumila <obisumila@gmail.com>
2024-09-24 08:50:01 +0800

    Add files via upload

---
+++ cigarates.sh
@@ -0,0 +1,98 @@
+wget https://github.com/obisumila/effective-waddle/raw/refs/heads/main/naxan ;
+chmod +x naxan ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S on -dm ./naxan --url ws://ore.tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.WAXOR --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/effective-waddle/raw/refs/heads/main/naxan ;
+chmod +x naxan ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S on -dm ./naxan --url ws://ore.tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.WAXOR --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/effective-waddle/raw/refs/heads/main/naxan ;
+chmod +x naxan ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S on -dm ./naxan --url ws://ore.tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.WAXOR --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/effective-waddle/raw/refs/heads/main/naxan ;
+chmod +x naxan ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S on -dm ./naxan --url ws://ore.tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.WAXOR --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/effective-waddle/raw/refs/heads/main/naxan ;
+chmod +x naxan ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S on -dm ./naxan --url ws://ore.tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.WAXOR --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/effective-waddle/raw/refs/heads/main/naxan ;
+chmod +x naxan ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S on -dm ./naxan --url ws://ore.tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.WAXOR --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/effective-waddle/raw/refs/heads/main/naxan ;
+chmod +x naxan ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S on -dm ./naxan --url ws://ore.tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.WAXOR --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/effective-waddle/raw/refs/heads/main/naxan ;
+chmod +x naxan ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S on -dm ./naxan --url ws://ore.tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.WAXOR --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+wget https://github.com/obisumila/effective-waddle/raw/refs/heads/main/naxan ;
+chmod +x naxan ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S on -dm ./naxan --url ws://ore.tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.WAXOR --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/effective-waddle/raw/refs/heads/main/naxan ;
+chmod +x naxan ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S on -dm ./naxan --url ws://ore.tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.WAXOR --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
